1e2c1592b4a89d6d96f490cf338599ab5b64f0d6
Luca Lambiase <luca.lambia@gmail.com>
2023-11-22 12:11:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,212 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            //conversazione attiva (indice dell'array contacts)
+            conversazioneAttiva: 1,
+            //contacts: array di oggetti
+            contacts: [
+                //contacts[x]: il singolo oggetto rappresenta una conversazione
+                {
+                    //contact[x].name: è il nome della persona della conversazione
+                    name: 'Michele',
+                    avatar: './img/avatar_1.jpg',
+                    visible: true,
+                    //contacts[x].messages: è un array di oggetti che rappresenta i messaggi scambiati
+                    messages: [
+                        //contacts[x].messages[y]: il singolo oggetto rappresenta un messaggio
+                        {
+                            date: '10/01/2020 15:30:55',
+                            //contacts[x].messages[y].message: è il testo del singolo messaggio
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                }, {
+                    name: 'Fabio',
+                    avatar: './img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                }, {
+                    name: 'Samuele',
+                    avatar: './img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                }, {
+                    name: 'Alessandro B.',
+                    avatar: './img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                }, {
+                    name: 'Alessandro L.',
+                    avatar: './img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                }, {
+                    name: 'Claudia',
+                    avatar: './img/avatar_6.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao Claudia, hai novità?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Non ancora',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'Nessuna nuova, buona nuova',
+                            status: 'sent'
+                        }
+                    ],
+                }, {
+                    name: 'Federico',
+                    avatar: './img/avatar_7.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            status: 'received'
+                        }
+                    ],
+                }, {
+                    name: 'Davide',
+                    avatar: './img/avatar_8.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                }
+            ]
+        }
+    },
+    methods: {
+        selezionaChat(n) {
+            console.log(n);
+        }
+    },
+    mounted() {
+        // stampiamo tutto l'array delle conversazioni
+        // console.log( this.contacts );
+
+        // stampiamo tutti i nomi delle conversazioni
+        // this.contacts.forEach(conversazione => {
+        //     console.log( "foreach: " + conversazione.name );
+        // });
+
+        // stampiamo tutti i nomi delle conversazioni
+        // for (let i = 0; i < this.contacts.length; i++) {
+        //     console.log( "for" + this.contacts[i].name );
+        // }
+
+        // stampiamo tutti i nomi delle conversazioni
+        // for (const conversazione of this.contacts) {
+        //     console.log( "forof: " + conversazione.name );
+        // }
+
+        // stampiamo tutti i nomi delle conversazioni
+        // for (const i in this.contacts) {
+        //     console.log( "forin: " + this.contacts[i].name );
+        // }
+
+        // stampiamo il nome di una specifica conversazione (0)
+        // console.log( this.contacts[0].name );
+
+        // stampiamo l'array messaggi di una specifica conversazione (0)
+        // console.log( this.contacts[0].messages );
+
+        // cicliamo sull'array messaggi di una specifica conversazione (0)
+        this.contacts[0].messages.forEach(messaggio => {
+            console.log(messaggio.message);
+        });
+    }
+}).mount('#app')
